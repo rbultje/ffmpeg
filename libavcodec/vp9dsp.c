f401431f1ccb06e7716dd34b1e831a92fc7b8698
Ronald S. Bultje <rsbultje@gmail.com>
2013-08-06 06:40:58 +0800

    Fix bugs in dc_top/dc_left 8x8/16x16 intra prediction.
    Top half of vp90-02-size-64x64.webm decodes correctly now.

--- libavcodec/vp9dsp.c
+++ libavcodec/vp9dsp.c
@@ -237,7 +237,7 @@ static void dc_left_16x16_c(uint8_t *dst, ptrdiff_t stride,
     uint64_t dc = 0x0101010101010101ULL *
         ((left[0] + left[1] + left[2] + left[3] + left[4] + left[5] +
           left[6] + left[7] + left[8] + left[9] + left[10] + left[11] +
-          left[12] + left[13] + left[14] + left[15] + 4) >> 3);
+          left[12] + left[13] + left[14] + left[15] + 8) >> 4);
     int y;
 
     for (y = 0; y < 16; y++) {
@@ -263,7 +263,7 @@ static void dc_top_8x8_c(uint8_t *dst, ptrdiff_t stride,
 {
     uint64_t dc = 0x0101010101010101ULL *
         ((top[0] + top[1] + top[2] + top[3] +
-          top[4] + top[5] + top[6] + top[7] + 2) >> 2);
+          top[4] + top[5] + top[6] + top[7] + 4) >> 3);
     int y;
 
     for (y = 0; y < 8; y++) {
@@ -278,7 +278,7 @@ static void dc_top_16x16_c(uint8_t *dst, ptrdiff_t stride,
     uint64_t dc = 0x0101010101010101ULL *
         ((top[0] + top[1] + top[2] + top[3] + top[4] + top[5] +
           top[6] + top[7] + top[8] + top[9] + top[10] + top[11] +
-          top[12] + top[13] + top[14] + top[15] + 2) >> 2);
+          top[12] + top[13] + top[14] + top[15] + 8) >> 4);
     int y;
 
     for (y = 0; y < 16; y++) {
