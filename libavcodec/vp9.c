0b7bce64edf445041ddf5a436aeb18266789850b
Ronald S. Bultje <rsbultje@gmail.com>
2013-08-08 18:11:31 +0800

    Semi-rewrite sub-exponential diff probability update reading.

--- libavcodec/vp9.c
+++ libavcodec/vp9.c
@@ -191,74 +191,58 @@ static int update_size(AVCodecContext *ctx, int w, int h)
 }
 
 // for some reason the sign bit is at the end, not the start, of a bit sequence
-static inline int get_sbits_inv(GetBitContext *gb, int n)
+static av_always_inline int get_sbits_inv(GetBitContext *gb, int n)
 {
     int v = get_bits(gb, n);
     return get_bits1(gb) ? -v : v;
 }
 
-static int inv_recenter_nonneg(int v, int m) {
+static av_always_inline int inv_recenter_nonneg(int v, int m)
+{
     return v > 2 * m ? v : v & 1 ? m - (v + 1) / 2 : m + v / 2;
-}
-
-static int inv_remap_prob(int v, int m) {
-    static const int inv_map_table[254] = {
-          6,  19,  32,  45,  58,  71,  84,  97, 110, 123, 136, 149, 162, 175,
-        188, 201, 214, 227, 240, 253,   0,   1,   2,   3,   4,   5,   7,   8,
-          9,  10,  11,  12,  13,  14,  15,  16,  17,  18,  20,  21,  22,  23,
-         24,  25,  26,  27,  28,  29,  30,  31,  33,  34,  35,  36,  37,  38,
-         39,  40,  41,  42,  43,  44,  46,  47,  48,  49,  50,  51,  52,  53,
-         54,  55,  56,  57,  59,  60,  61,  62,  63,  64,  65,  66,  67,  68,
-         69,  70,  72,  73,  74,  75,  76,  77,  78,  79,  80,  81,  82,  83,
-         85,  86,  87,  88,  89,  90,  91,  92,  93,  94,  95,  96,  98,  99,
-        100, 101, 102, 103, 104, 105, 106, 107, 108, 109, 111, 112, 113, 114,
-        115, 116, 117, 118, 119, 120, 121, 122, 124, 125, 126, 127, 128, 129,
-        130, 131, 132, 133, 134, 135, 137, 138, 139, 140, 141, 142, 143, 144,
-        145, 146, 147, 148, 150, 151, 152, 153, 154, 155, 156, 157, 158, 159,
-        160, 161, 163, 164, 165, 166, 167, 168, 169, 170, 171, 172, 173, 174,
-        176, 177, 178, 179, 180, 181, 182, 183, 184, 185, 186, 187, 189, 190,
-        191, 192, 193, 194, 195, 196, 197, 198, 199, 200, 202, 203, 204, 205,
-        206, 207, 208, 209, 210, 211, 212, 213, 215, 216, 217, 218, 219, 220,
-        221, 222, 223, 224, 225, 226, 228, 229, 230, 231, 232, 233, 234, 235,
-        236, 237, 238, 239, 241, 242, 243, 244, 245, 246, 247, 248, 249, 250,
-        251, 252,
-    };
-    v = inv_map_table[v] + 1;
-    m--;
-    if ((m << 1) <= 255) {
-        return 1 + inv_recenter_nonneg(v, m);
-    } else {
-        return 255 - inv_recenter_nonneg(v, 254 - m);
-    }
-}
-
-static int decode_uniform(VP56RangeCoder *c, int range) {
-    int v, bits = av_ceil_log2(range);
-    int lower = (1 << bits) - range;
-    if (!bits)  // FIXME can this happen?
-        return 0;
-    
-    v = vp8_rac_get_uint(c, bits - 1);
-    return v < lower ? v : (v << 1) - lower + vp8_rac_get(c);
 }
 
 // differential forward probability updates
 static int update_prob(VP56RangeCoder *c, int p)
 {
-    int inc = 0, bits = 4, base = 0;
-    for (;;) {
-        int range = 1 << bits;
-        if (255 <= base + 3 * range) {
-            // FIXME: unroll loop to inline this if
-            return inv_remap_prob(decode_uniform(c, 255 - base) + base, p);
-        } else if (!vp8_rac_get(c)) {
-            return inv_remap_prob(vp8_rac_get_uint(c, bits) + base, p);
-        } else {
-            base += range;
-            bits += inc;
-            inc = 1;
-        }
+    static const int inv_map_table[254] = {
+          7,  20,  33,  46,  59,  72,  85,  98, 111, 124, 137, 150, 163, 176,
+        189, 202, 215, 228, 241, 254,   1,   2,   3,   4,   5,   6,   8,   9,
+         10,  11,  12,  13,  14,  15,  16,  17,  18,  19,  21,  22,  23,  24,
+         25,  26,  27,  28,  29,  30,  31,  32,  34,  35,  36,  37,  38,  39,
+         40,  41,  42,  43,  44,  45,  47,  48,  49,  50,  51,  52,  53,  54,
+         55,  56,  57,  58,  60,  61,  62,  63,  64,  65,  66,  67,  68,  69,
+         70,  71,  73,  74,  75,  76,  77,  78,  79,  80,  81,  82,  83,  84,
+         86,  87,  88,  89,  90,  91,  92,  93,  94,  95,  96,  97,  99, 100,
+        101, 102, 103, 104, 105, 106, 107, 108, 109, 110, 112, 113, 114, 115,
+        116, 117, 118, 119, 120, 121, 122, 123, 125, 126, 127, 128, 129, 130,
+        131, 132, 133, 134, 135, 136, 138, 139, 140, 141, 142, 143, 144, 145,
+        146, 147, 148, 149, 151, 152, 153, 154, 155, 156, 157, 158, 159, 160,
+        161, 162, 164, 165, 166, 167, 168, 169, 170, 171, 172, 173, 174, 175,
+        177, 178, 179, 180, 181, 182, 183, 184, 185, 186, 187, 188, 190, 191,
+        192, 193, 194, 195, 196, 197, 198, 199, 200, 201, 203, 204, 205, 206,
+        207, 208, 209, 210, 211, 212, 213, 214, 216, 217, 218, 219, 220, 221,
+        222, 223, 224, 225, 226, 227, 229, 230, 231, 232, 233, 234, 235, 236,
+        237, 238, 239, 240, 242, 243, 244, 245, 246, 247, 248, 249, 250, 251,
+        252, 253,
+    };
+    int d;
+
+    if (!vp8_rac_get(c)) {
+        d = vp8_rac_get_uint(c, 4) + 0;
+    } else if (!vp8_rac_get(c)) {
+        d = vp8_rac_get_uint(c, 4) + 16;
+    } else if (!vp8_rac_get(c)) {
+        d = vp8_rac_get_uint(c, 5) + 32;
+    } else {
+        d = vp8_rac_get_uint(c, 7);
+        if (d >= 65)
+            d = (d << 1) - 65 + vp8_rac_get(c);
+        d += 64;
     }
+
+    return p <= 128 ? 1 + inv_recenter_nonneg(inv_map_table[d], p - 1) :
+                    255 - inv_recenter_nonneg(inv_map_table[d], 255 - p);
 }
 
 #define VP9_SYNCCODE 0x498342
